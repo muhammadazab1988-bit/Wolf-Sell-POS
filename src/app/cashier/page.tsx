b4effd28edbdc87bd4df65a8691b5a968fe88286
MO-AZAB <muhammadazab1988@gmail.com>
2026-07-17 23:09:46 +0300

    Fix missing LuCheck import

--- src/app/cashier/page.tsx
+++ src/app/cashier/page.tsx
@@ -9,6 +9,7 @@ import BillingHistory from "@/components/BillingHistory";
 import Sidebar from "@/components/Sidebar";
 import ClothingCart, { type CartItem } from "@/components/ClothingCart";
 import Header from "@/components/Header";
+import { LuScanBarcode, LuCheck } from "react-icons/lu";
 
 type Payment = {
   orderNumber: number;
